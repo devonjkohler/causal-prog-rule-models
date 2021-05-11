eb78675ec87e37200bdea5be60b70fcac42a67a1
Jeremy Zucker <djinnome@gmail.com>
2021-05-11 15:25:50 -0700

    updated notebook

--- src/probability_trees.py
+++ src/probability_trees.py
@@ -815,3 +815,15 @@ class PTree:
       res = self._fromFunc(func, new_bvars)
       children.append(self.child(prob, statement, res))
     return children
+
+
+class PDF(object):
+  def __init__(self, pdf, size=(200,200)):
+    self.pdf = pdf
+    self.size = size
+
+  def _repr_html_(self):
+    return '<iframe src={0} width={1[0]} height={1[1]}></iframe>'.format(self.pdf, self.size)
+
+  def _repr_latex_(self):
+    return r'\includegraphics[width=1.0\textwidth]{{{0}}}'.format(self.pdf)
